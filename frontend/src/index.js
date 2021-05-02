a352f997f3b16c8e499fc9336f378ae075851a3f
Charlie Merrell <charlie.merrell20@gmail.com>
2021-05-02 17:27:09 +0100

    makes multiple cols on larger screens in list all mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/small.scss";
+import "./styles/medium.scss";
+import "./styles/large.scss";
 
 ReactDOM.render(
     <React.StrictMode>
